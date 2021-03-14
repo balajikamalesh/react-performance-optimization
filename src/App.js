166954da540eb56b482b9876d6f46075948c819f
Balaji <abalajikamalesh@gmail.com>
2021-03-15 00:11:37 +0530

    Code splitting implemented using React.lazy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { Suspense } from 'react';
 import react from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
-import AsyncComponent from './components/AsyncComponent';
+
+const Page2Lazy = React.lazy(() => import('./components/Page2'));
+const Page3Lazy = React.lazy(() => import('./components/Page3'));
 
 class App extends react.Component {
   constructor(){
@@ -21,11 +24,13 @@ class App extends react.Component {
     if(this.state.route === 'page1'){
       return <Page1 onRouteChange={this.onRouteChange}/>
     } else if(this.state.route === 'page2') {
-      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
-      return <AsyncPage2 onRouteChange={this.onRouteChange}/>
+      return <Suspense fallback={<div>Loading....</div>}>
+          <Page2Lazy onRouteChange={this.onRouteChange}/>
+        </Suspense>
     } else if(this.state.route === 'page3'){
-      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
-      return <AsyncPage3 onRouteChange={this.onRouteChange}/>
+      return <Suspense fallback={<div>Loading....</div>}>
+        <Page3Lazy onRouteChange={this.onRouteChange}/>
+      </Suspense>
     }
   }
 }
